0e612291cfc0ea0e97106e53e21c50d02c20958e
ThisIsPIRI <thisispiri@gmail.com>
2018-08-02 03:36:58 +0900

    Downsize in parallel

--- image_downsizer.py
+++ image_downsizer.py
@@ -1,4 +1,5 @@
 """Downsize all images in a directory and saves them in another with ImageMagick."""
+# TODO: Replace ImageMagick with something that can be called faster(without making subprocesses)
 from os import listdir
 from os.path import isfile, join
 import re
@@ -12,4 +13,8 @@ dimension = input("Resulting width: ") + 'x' + input("Resulting height:") + "!" 
 images = [join(dir_from, i).replace('\\', '\\\\') for i in listdir(dir_from) if isfile(join(dir_from, i)) and re.match(".*\." + extension, i, re.IGNORECASE)]  # TODO: support multiple extensions at once
 for index, image in enumerate(images):
 	print(image)
-	subprocess.run(["magick", "convert", image, "-resize", dimension, join(dir_to, f"{index}.jpg")])
+	if index % 12 == 11:  # Wait for every 12th process to finish to avoid opening too many at once.
+		subprocess.run(["magick", "convert", image, "-resize", dimension, join(dir_to, f"{index}.jpg")])
+		print(f"{(index + 1)}/{len(images)}")
+	else:
+		subprocess.Popen(["magick", "convert", image, "-resize", dimension, join(dir_to, f"{index}.jpg")])
