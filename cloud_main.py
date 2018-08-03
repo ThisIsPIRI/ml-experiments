c41eac8f0054da9039e7c4fd11bd613e67727697
ThisIsPIRI <thisispiri@gmail.com>
2018-08-04 07:40:47 +0900

    Create split_data.py

--- cloud_main.py
+++ cloud_main.py
@@ -5,10 +5,11 @@ import tensorflow as tf
 
 def main():
 	IMG_NUM = 2256
-	images = np.empty(IMG_NUM, dtype=np.ndarray)
+	d_images = np.empty(IMG_NUM, dtype=np.ndarray)
 	for i in range(IMG_NUM):
-		images[i] = plt.imread(f"C:\CloudData\{i}.jpg", format="jpeg")
-	print(images)
+		d_images[i] = plt.imread(f"C:\CloudData\{i}.jpg", format="jpeg")
+	t_images = tf.placeholder(dtype=tf.float32)
+	dataset = tf.data.Dataset.from_tensors(t_images)
 
 
 if __name__ == "__main__":
