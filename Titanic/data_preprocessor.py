2c1c2d07946ef8c29cea822feab9a3ff34434860
ThisIsPIRI <thisispiri@gmail.com>
2018-08-05 10:01:24 +0900

    Replace try-catch for blank 'class' removal with a subclass

--- Titanic/data_preprocessor.py
+++ Titanic/data_preprocessor.py
@@ -9,6 +9,16 @@ def is_number(it):
 	return True
 
 
+class SafeSet(set):
+	def safe_remove(self, element):
+		"""Removes element. Does not throw a KeyError even if element isn't present. Returns the SafeSet itself."""
+		try:
+			self.remove(element)
+		except KeyError:
+			pass
+		return self
+
+
 def main():
 	with open("train.csv") as f_original, open("processed_train.csv", 'w', newline='') as f_processed:
 		reader = csv.reader(f_original)
@@ -20,12 +30,7 @@ def main():
 					continue
 				d_processed.append(column)
 			elif column[0] != "Name" and column[0] != "Ticket" and column[0] != "Cabin":  # Convert non-numerical features to indicator variables
-				classes = set(column[1:])
-				print(classes)
-				try: classes.remove('')  # Remove the empty 'class'
-				except KeyError: pass
-				print(classes)
-				for c in classes:
+				for c in SafeSet(column[1:]).safe_remove(''):
 					d_processed.append([c] + [int(f == c) for f in column[1:]])
 		for r in zip(*d_processed):
 			writer.writerow(r)
