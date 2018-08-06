772389bf8b38df05676aaab566ac02e8cfc58243
ThisIsPIRI <thisispiri@gmail.com>
2018-08-06 09:10:22 +0900

    Replace HParams with a raw str

--- Titanic/data_preprocessor.py
+++ Titanic/data_preprocessor.py
@@ -1,5 +1,4 @@
 import csv
-from tensorflow.contrib.training import HParams
 
 
 def is_number(it):
@@ -21,26 +20,31 @@ class SafeSet(set):
 
 
 def main():
-	hparams = HParams()
-	with open("train.csv") as f_original, open("processed_train.csv", 'w', newline='') as f_processed:
+	hparams = '{'  # The JSON string of all divisors
+	result_transposed = []
+	# Read and process. Assumes column[0] contains the name of the column.
+	with open("train.csv") as f_original:
 		reader = csv.reader(f_original)
-		writer = csv.writer(f_processed)
-		result_transposed = []
 		to_discard = {"PassengerId", "Name", "Ticket", "Cabin"}
 		for column in zip(*reader):
 			if column[0] in to_discard:
 				continue
 			if is_number(column[1]):  # TODO: Account for empty cells on the first row
 				float_column = [float(x) if is_number(x) else 0. for x in column[1:]]
-				hparams.add_hparam(column[0] + "_divisor", max(float_column) - min(float_column))
+				hparams += f"\"{column[0]}_divisor\": {max(float_column) - min(float_column)}, "
 				result_transposed.append([column[0]] + [x / (max(float_column) - min(float_column)) for x in float_column])
 			else:  # Convert non-numerical features to indicator variables.
 				for c in SafeSet(column[1:]).safe_remove(''):
 					result_transposed.append([c] + [int(f == c) for f in column[1:]])
+	# Write the data
+	with open("processed_train.csv", 'w', newline='') as f_processed:
+		writer = csv.writer(f_processed)
 		for r in zip(*result_transposed):
 			writer.writerow(r)
+	# Write the divisors
+	hparams = hparams[:-2] + '}'  # Remove the trailing comma
 	with open("ScalingDivisors.json", 'w', newline='') as f_hparams:
-		f_hparams.write(hparams.to_json())
+		f_hparams.write(hparams)
 
 
 if __name__ == "__main__":
