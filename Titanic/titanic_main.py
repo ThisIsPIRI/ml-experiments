0e4c9e8821172b2a13784795a0780e8bab2f9cb1
ThisIsPIRI <thisispiri@gmail.com>
2018-08-08 06:34:11 +0900

    Add raw linear_regression

--- Titanic/titanic_main.py
+++ Titanic/titanic_main.py
@@ -1,11 +1,16 @@
 import csv
+import numpy as np
+
+import linear_regression
 
 
 def main():
-	"""d_raw_train = []
-	with open("train.csv") as f_train:
+	with open("processed_train.csv") as f_train:
 		reader = csv.reader(f_train)
-		for column in zip(*reader):"""
+		int_converter = [[float(f) for f in l] for l in list(reader)[1:]]
+		d_train = np.array([list(r) for r in int_converter])
+		# TODO: read labels
+	linear_regression.train(d_train, d_labels)
 
 
 if __name__ == "__main__":
